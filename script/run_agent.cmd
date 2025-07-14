7d89399a06c45cbe28959d33bd0401ace3920293
alexvruden <98196594+alexvruden@users.noreply.github.com>
2025-07-14 18:55:58 +0300

    Update run_agent.cmd

--- script/run_agent.cmd
+++ script/run_agent.cmd
@@ -1,6 +1,8 @@
 @echo off
+setlocal EnableDelayedExpansion
 chcp 1251 >nul
 set "home=%~1"
+set "cmd_run_short="
 if "x%home%"=="x" exit
 if not exist %home%\run.cmd exit
 echo.unknown>%home%\bin\agent_status
@@ -11,12 +13,19 @@ set /a s1=0
 set /p mode=<%home%\bin\agent_mode
 set mode=%mode: =%
 set /p cmd_run=<%home%\bin\agent_start_cmd
+for /l %%a in (1,1,1000) do (
+	if "x!home:~%%a,1!"=="x" (
+		set cmd_run_short=!cmd_run:~%%a!
+		goto:@break
+	)
+)
+:@break
 if "x%mode%"=="xstop" goto:@stop
 if "x%mode%"=="xstart" goto:@start
 
 :@ping
 timeout /T 30 /NOBREAK >nul
-ping /n 1 one.one.one.one
+ping /n 1 one.one.one.one >nul
 if %errorlevel% neq 0 (
 	if "x%mode%"=="xstart" (
 		echo.no internet, restart>%home%\bin\agent_status
@@ -26,7 +35,7 @@ if %errorlevel% neq 0 (
 	) else echo.stoped and no internet>%home%\bin\agent_status
 ) else (
 	if %s0% equ 1 echo.stoped>%home%\bin\agent_status
-	if %s1% equ 1 echo.started '%cmd_run%' >%home%\bin\agent_status
+	if %s1% equ 1 echo.started '..%cmd_run_short%'>%home%\bin\agent_status
 )
 goto:@loop
 

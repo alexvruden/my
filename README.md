0a15c7ae439d9c04af21263b088aea51bcc5fa49
alexvruden <98196594+alexvruden@users.noreply.github.com>
2025-07-15 19:26:45 +0300

    fix

--- README.md
+++
@@ -1,4 +0,0 @@
-# my
-
-run
-<img src="https://github.com/alexvruden/my/blob/main/01.png" />
--- script/windivert_delete.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo DELETE WINDIVERT DRIVER
-sc qc windivert
-if "x%errorlevel%"=="x0" (
-	sc stop windivert
-	sc delete windivert
-)
-pause

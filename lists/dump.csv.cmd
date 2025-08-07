66b380f48bd423193ca40c3c0ce49b839b74f8f2
alexvruden <98196594+alexvruden@users.noreply.github.com>
2025-08-07 22:28:05 +0300

    Update dump.csv.cmd

--- lists/dump.csv.cmd
+++ lists/dump.csv.cmd
@@ -2,65 +2,85 @@
 rem https://github.com/alexvruden/my
 chcp 1251 >nul
 setlocal EnableDelayedExpansion
+if "x%~1"=="x" (
+	echo.Пример запуска:
+	echo.
+	echo.dump.csv.cmd domain
+	echo.dump.csv.cmd ipv4
+	echo.dump.csv.cmd ipv6
+	echo.dump.csv.cmd all
+	goto:@exit
+)
+set /a cmddom=0, cmdipv4=0, cmdipv6=0, ipv46=0
+for %%i in (%~1 %~2 %~3 %~4) do (
+	if "x%%i"=="xdomain" set cmddom=1
+	if "x%%i"=="xipv4" set cmdipv4=1
+	if "x%%i"=="xipv6" set cmdipv6=1
+	if "x%%i"=="xall" set /a cmddom=1, cmdipv4=1, cmdipv6=1
+)
+set /a ipv46=%cmdipv4%+%cmdipv6%
 if not exist dump.csv (
 	echo.Скачать архив и распаковать список.
 	echo.https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv.gz
-	echo.
-	pause
-	exit
+	goto:@exit
 )
 (set LF=^
 %=EMPTY=%
 )
-<nul set /p =#!LF!>dump.csv.domain.txt
-<nul set /p =#!LF!>dump.csv.ipv4.txt
-<nul set /p =#!LF!>dump.csv.ipv6.txt
-set /a count=0
-set /a total=0
+set /p foo=<dump.csv
+if %cmddom% neq 0 <nul set /p =#!foo!!LF!>dump.csv.domain.txt
+if %cmdipv4% neq 0 <nul set /p =#!foo!!LF!>dump.csv.ipv4.txt
+if %cmdipv6% neq 0 <nul set /p =#!foo!!LF!>dump.csv.ipv6.txt
+set /a count=0, total=0, count_dom=0, count_ipv4=0, count_ipv6=0
 set "str_dom="
-set /a count_dom=0
 set "str_ipv4="
-set /a count_ipv4=0
 set "str_ipv6="
-set /a count_ipv6=0
 echo.Парсинг 'dump.csv'
 echo.
 <nul set /p =[2K[3;0H[?25lЧитаем список...
 for /f "skip=1" %%a in (dump.csv) do set /a total+=1
 call:progress_in_percent_begin %total%
 for /f "skip=1 tokens=1,2 delims=;" %%a in (dump.csv) do (
-	if not "x%%b"=="x" (
-		set "foo=%%b"
-		if "x!foo:~0,1!"=="x*" (
-			set "str_dom=!str_dom!!LF!!foo:~2!"
-		) else (
-			set "str_dom=!str_dom!!LF!!foo!"
-		)
-		set /a count_dom+=1
-		if not "x!str_dom:~8000,1!"=="x" (
-			<nul set /p =!str_dom!!LF!>>dump.csv.domain.txt
-			set "str_dom="
+	if %cmddom% neq 0 (
+		if not "x%%b"=="x" (
+			set "foo=%%b"
+			if "x!foo:~0,1!"=="x*" (
+				set "str_dom=!str_dom!!LF!!foo:~2!"
+			) else (
+				set "str_dom=!str_dom!!LF!!foo!"
+			)
+			set /a count_dom+=1
+			if not "x!str_dom:~8000,1!"=="x" (
+				<nul set /p =!str_dom!!LF!>>dump.csv.domain.txt
+				set "str_dom="
+			)
 		)
 	)
-	set "foo=%%a"
-	set "foo=!foo:|= !"
-	for %%i in (!foo!) do (
-		set goo=0
-		for /f %%x in ('echo.%%i ^| find "."') do set goo=1
-		if !goo! equ 1 (
-			set str_ipv4=!str_ipv4!!LF!%%i
-			set /a count_ipv4+=1
-			if not "x!str_ipv4:~8000,1!"=="x" (
-				<nul set /p =!str_ipv4!!LF!>>dump.csv.ipv4.txt
-				set "str_ipv4="
-			)
-		) else (
-			set str_ipv6=!str_ipv6!!LF!%%i
-			set /a count_ipv6+=1
-			if not "x!str_ipv6:~8000,1!"=="x" (
-				<nul set /p =!str_ipv6!!LF!>>dump.csv.ipv6.txt
-				set "str_ipv6="
-				set /a show=1
+	if %ipv46% neq 0 (
+		set "foo=%%a"
+		set "foo=!foo:|= !"
+		for %%i in (!foo!) do (
+			set goo=0
+			for /f %%x in ('echo.%%i ^| find "."') do set goo=1
+			if !goo! equ 1 (
+				if %cmdipv4% neq 0 (
+					set str_ipv4=!str_ipv4!!LF!%%i
+					set /a count_ipv4+=1
+					if not "x!str_ipv4:~8000,1!"=="x" (
+						<nul set /p =!str_ipv4!!LF!>>dump.csv.ipv4.txt
+						set "str_ipv4="
+					)
+				)
+			) else (
+				if %cmdipv6% neq 0 (
+					set str_ipv6=!str_ipv6!!LF!%%i
+					set /a count_ipv6+=1
+					if not "x!str_ipv6:~8000,1!"=="x" (
+						<nul set /p =!str_ipv6!!LF!>>dump.csv.ipv6.txt
+						set "str_ipv6="
+						set /a show=1
+					)
+				)
 			)
 		)
 	)
@@ -70,9 +90,25 @@ for /f "skip=1 tokens=1,2 delims=;" %%a in (dump.csv) do (
 )
 <nul set /p =[2K[3;0H[?25lГотово
 echo.
-echo.Количество доменов : %count_dom%
-echo.Количество ipv4    : %count_ipv4%
-echo.Количество ipv6    : %count_ipv6%
+if %cmddom% neq 0  echo.Количество доменов : %count_dom%
+if %cmdipv4% neq 0 echo.Количество ipv4    : %count_ipv4%
+if %cmdipv6% neq 0 echo.Количество ipv6    : %count_ipv6%
+tar -h 1>nul 2>&1
+if %errorlevel% equ 0 (
+	if %cmddom% neq 0 (
+		tar -czf dump.csv.domain.gz dump.csv.domain.txt 1>nul 2>&1
+		if !errorlevel! neq 0 del /F /Q dump.csv.domain.gz >nul
+	)
+	if %cmdipv4% neq 0 (
+		tar -czf dump.csv.ipv4.gz dump.csv.ipv4.txt 1>nul 2>&1
+		if !errorlevel! neq 0 del /F /Q dump.csv.ipv4.gz >nul
+	)
+	if %cmdipv6% neq 0 (
+		tar -czf dump.csv.ipv6.gz dump.csv.ipv6.txt 1>nul 2>&1
+		if !errorlevel! neq 0 del /F /Q dump.csv.ipv6.gz >nul
+	)
+)
+:@exit
 echo.
 echo.Нажмите любую клавишу для выхода.
 pause >nul
